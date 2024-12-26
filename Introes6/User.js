fbbb1f029e7f831cc49895f1d5be1ed512db5746
harshitgupta0032 <harshitkumargupta03.hp@gmail.com>
2024-12-26 15:40:50 +0530

    complete modules

---
+++ Introes6/User.js
@@ -0,0 +1,5 @@
+const name = "harshit gupta";
+const email = "abc@gmail.com";
+const number = 2342434;
+
+export { name, email , number};
---
+++ Introes6/modules.js
@@ -0,0 +1,6 @@
+// modules are used for import file and export data , variable object and function 
+console.log("");
+import { name, email, number } from "./User.js"
+console.log("name : " + name)
+console.log("email : "+email)
+console.log("number : "+number)
